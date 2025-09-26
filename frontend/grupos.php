2b53eb86f568b2750e034e4fa9328c762df72ddf
Nehuel Acosta <nehuelacostarodriguez@gmail.com>
2025-09-26 19:22:40 -0300

    ADD: Codificacion del arbol de navegacion (casi) terminada

---
+++ frontend/grupos.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grupos</title>
+</head>
+<body>
+    <h2>Grupos</h2>
+    <p>Aca va a haber una lista con los grupos</p>
+    <p>Se podrá seleccionar un grupo para ver sus integrantes y su horario</p>
+    <br>
+    <a href="menuRecursos.php">Volver</a>
+</body>
+</html>
