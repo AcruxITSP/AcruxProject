9c59e2e03cc9efb890aa88de915698fc451c4962
sofiaverocai <sofiaverocai10@gmail.com>
2025-09-30 09:58:24 -0300

    estilos pagina recursos, especialmente aulas y funcionarios

--- frontend/aulas.php
+++ frontend/aulas.php
@@ -9,47 +9,44 @@ require 'scriptsPhp/globalFunctions.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrar funcionarios</title>
-          <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/styles.css">
 </head>
 
 <body>
-    <style>
-        input, select {
-            margin-bottom: 1rem;
-        }
-    </style>
-    <form id="ingresarAulas-Form" action="scriptsPhp/scriptRegistrarAulas.php" method="post">
+    <div class="forms-container">
+        <div class="form-card">
+            <h2>Registrar Aulas</h2>
+            <form id="ingresarAulas-Form" action="scriptsPhp/scriptRegistrarAulas.php" method="post">
 
-        <h3>Registrar Aulas</h3>
+                <label for="codigo">Código:</label>
+                <input type="text" id="codigo" name="codigo" required>
 
-        <label for="codigo">Código:</label><br>
-        <input type="text" id="codigo" name="codigo" required><br>
+                <label for="piso">Piso:</label>
+                <select id="piso" name="piso" required>
+                    <option value="PB">PB</option>
+                    <option value="1er">1er</option>
+                    <option value="2do">2do</option>
+                </select>
 
-        <label>Piso:</label><br>
-        <select name="piso" required>
-            <option value="PB">PB</option>
-            <option value="1er">1er</option>
-            <option value="2do">2do</option>
-        </select><br>
+                <label for="proposito">Propósito:</label>
+                <select id="proposito" name="proposito" required>
+                    <option value="General">General</option>
+                    <option value="Laboratorio">Laboratorio</option>
+                    <option value="Taller">Taller</option>
+                    <option value="Aula Informatica">Aula Informática</option>
+                    <option value="General">Otro (Aun no implementado)</option>
+                </select>
 
-        <label>Propósito:</label><br>
-        <select name="proposito" required>
-            <option value="General">General</option>
-            <option value="Laboratorio">Laboratorio</option>
-            <option value="Taller">Taller</option>
-            <option value="Aula Informatica">Aula Informática</option>
-            <option value="General">Otro (Aun no implementado)</option>
-        </select><br>
+                <label for="cantidadSillas">Capacidad (cantidad de sillas):</label>
+                <input type="number" id="cantidadSillas" name="cantidadSillas" required>
 
-        <label for="cantidadSillas">Capacidad (cantidad de sillas):</label><br>
-        <input type="number" id="cantidadSillas" name="cantidadSillas" required><br>
+                <input type="submit" value="Registrar">
+            </form>
 
-        <input type="submit">
-    </form>
-    <br>
-    <a href="reservasAulas.php">Reservar un aula</a>
-    <br><br>
-    <a href="menuRecursos.php">Volver</a>
+            <a href="reservasAulas.php">Reservar un aula</a><br>
+            <a href="menuRecursos.php">Volver</a>
+        </div>
+    </div>
 </body>
 
 </html>
