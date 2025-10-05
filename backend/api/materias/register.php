6e818f887b5b705212c58092edad17349d4ecb19
AcruxDevelopment <acruxstudios.dev@gmail.com>
2025-10-05 13:29:10 -0300

    Added `api/materias/register.php`

---
+++ backend/api/materias/register.php
@@ -0,0 +1,30 @@
+<?php
+@session_start();
+require_once dirname(__FILE__).'/../../utils/sql.php';
+require_once dirname(__FILE__).'/../../utils/respuestas.php';
+require_once dirname(__FILE__).'/../../utils/normalizeTitle.php';
+require_once dirname(__FILE__).'/../../db/connection.php';
+require_once dirname(__FILE__).'/../../models/Materia.php';
+require_once dirname(__FILE__).'/../_auth/roles.php';
+require_once dirname(__FILE__).'/../_auth/redirects.php';
+
+enum MateriaRegisterError : string
+{
+    case MISSING_PERMISSIONS = "MATERIA_REGISTER_MISSING_PERMISSIONS";
+}
+
+if($_SERVER['REQUEST_METHOD'] !== "POST")
+{
+    die();
+}
+// if(!isLoginAdscripta() && !isLoginAdministrador()) sendRedirectResponse("login.php");
+$con = connectDb();
+
+$nombre = $_POST['nombre'] ?? null;
+$nombre = normalizeTitle($nombre);
+
+$createMateriaResult = Materia::create($con, $nombre);
+if(!($createMateriaResult instanceof Materia)) Respuestas::enviarError($createMateriaResult);
+
+Respuestas::enviarOk();
+?>
