b9ff46d2221aec3162ede3221842dc709299ab9a
AcruxDevelopment <acruxstudios.dev@gmail.com>
2025-10-06 19:07:04 -0300

    FRONT: Login

---
+++ frontend/paginas/logIn.js
@@ -0,0 +1,33 @@
+const loginForm = document.getElementById("logInForm");
+
+loginForm.addEventListener('submit', async event => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    let response = await Users.loginWith(formData);
+    response = await response.json();
+
+    if(!response.ok)
+    {
+        if(response.value.type == "PERSONA_NOT_FOUND")
+        Swal.fire({
+            icon: "error",
+            title: "Login",
+            text: "No se encontro el usuario.",
+        });
+
+        if(response.value == "LOGIN_INVALID_PASSWORD")
+        Swal.fire({
+            icon: "error",
+            title: "Login",
+            text: "La contrasena es incorrecta.",
+        });
+        return;
+    }
+    else
+    {
+        location.replace("index.php");
+    }
+
+    response = await response;
+
+});
