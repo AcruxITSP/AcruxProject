bd7e511d8d6fa223edfe4af27563897c7a68bb1c
AcruxDevelopment <acruxstudios.dev@gmail.com>
2025-10-06 07:30:14 -0300

    Added `api/recursoexterno/register.php` (WIP)

---
+++ backend/api/recursoexterno/register.php
@@ -0,0 +1,33 @@
+<?php
+@session_start();
+require_once dirname(__FILE__).'/../../utils/sql.php';
+require_once dirname(__FILE__).'/../../utils/respuestas.php';
+require_once dirname(__FILE__).'/../../utils/textNormalizer.php';
+require_once dirname(__FILE__).'/../../db/connection.php';
+require_once dirname(__FILE__).'/../../models/RecursoExterno.php';
+require_once dirname(__FILE__).'/../_auth/roles.php';
+require_once dirname(__FILE__).'/../_auth/redirects.php';
+
+enum RecursoExtRegisterError : string
+{
+    case MISSING_PERMISSIONS = "RECURSOEXT_REGISTER_MISSING_PERMISSIONS";
+}
+
+if($_SERVER['REQUEST_METHOD'] !== "POST")
+{
+    die();
+}
+
+// TODO: APLICAR LUEGO DE TERMINAR REGISTERS
+// if(!isLoginAdscripta() && !isLoginAdministrador()) sendRedirectResponse("login.php");
+$con = connectDb();
+
+// TODO: VALIDAR
+$tipo = $_POST['tipo'] ?? null;
+$tipo = normalizeTitle($tipo);
+
+$createRecursoResult = RecursoExterno::create($con, $tipo, 1, disponible:true);
+if(!($createRecursoResult instanceof RecursoExterno)) Respuestas::enviarError($createRecursoResult);
+
+Respuestas::enviarOk();
+?>
