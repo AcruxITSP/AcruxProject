2336b14722d70fbc623e3f921b275a20519d9af4
Nehuel Acosta <nehuelacostarodriguez@gmail.com>
2025-10-26 18:27:22 -0300

    ADD: Funcion crear grupo

--- frontend/grupos/ver.php
+++ frontend/grupos/ver.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ver Grupos</title>
     <link rel="stylesheet" href="../styles/styles.css">
 </head>
+
 <body id="body-grupos-ver">
     <main id="main-grupos-ver">
         <div class="scrollable-list" id="lista-targetas-grupos">
             <div name="targeta-grupo">
                 <div name="encabezado">
                     <div class="marco-imagen"></div>
-                    <h1>3MD</h1>
+                    <h1 class="nombre-grupo">3MD</h1>
                 </div>
                 <div name="info-grupo">
-                    <p><strong>Adscripto/a: </strong></p> 
-                    <p>Silvana Salvatierra</p>
+                    <p><strong>Adscripto/a: </strong></p>
+                    <p class="nombre-adscripta">Silvana Salvatierra</p>
 
-                    <p><strong>Curso: </strong></p> 
-                    <p>Informática</p>
+                    <p><strong>Curso: </strong></p>
+                    <p class="nombre-curso">Informática</p>
                 </div>
                 <div name="botones">
                     <input type="button" name="editar" value="E">
@@ -29,14 +31,14 @@
             <div name="targeta-grupo">
                 <div name="encabezado">
                     <div class="marco-imagen"></div>
-                    <h1>3MD</h1>
+                    <h1 class="nombre-grupo">3MD</h1>
                 </div>
                 <div name="info-grupo">
-                    <p><strong>Adscripto/a: </strong></p> 
-                    <p>Silvana Salvatierra</p>
-                    
-                    <p><strong>Curso: </strong></p> 
-                    <p>Informática</p>
+                    <p><strong>Adscripto/a: </strong></p>
+                    <p class="nombre-adscripta">Silvana Salvatierra</p>
+
+                    <p><strong>Curso: </strong></p>
+                    <p class="nombre-curso">Informática</p>
                 </div>
                 <div name="botones">
                     <input type="button" name="editar" value="E">
@@ -46,14 +48,14 @@
             <div name="targeta-grupo">
                 <div name="encabezado">
                     <div class="marco-imagen"></div>
-                    <h1>3MD</h1>
+                    <h1 class="nombre-grupo">3MD</h1>
                 </div>
                 <div name="info-grupo">
-                    <p><strong>Adscripto/a: </strong></p> 
-                    <p>Silvana Salvatierra</p>
-                    
-                    <p><strong>Curso: </strong></p> 
-                    <p>Informática</p>
+                    <p><strong>Adscripto/a: </strong></p>
+                    <p class="nombre-adscripta">Silvana Salvatierra</p>
+
+                    <p><strong>Curso: </strong></p>
+                    <p class="nombre-curso">Informática</p>
                 </div>
                 <div name="botones">
                     <input type="button" name="editar" value="E">
@@ -63,24 +65,47 @@
             <div name="targeta-grupo">
                 <div name="encabezado">
                     <div class="marco-imagen"></div>
-                    <h1>3MD</h1>
+                    <h1 class="nombre-grupo">3MD</h1>
                 </div>
                 <div name="info-grupo">
-                    <p><strong>Adscripto/a: </strong></p> 
-                    <p>Silvana Salvatierra</p>
-                    
-                    <p><strong>Curso: </strong></p> 
-                    <p>Informática</p>
+                    <p><strong>Adscripto/a: </strong></p>
+                    <p class="nombre-adscripta">Silvana Salvatierra</p>
+
+                    <p><strong>Curso: </strong></p>
+                    <p class="nombre-curso">Informática</p>
                 </div>
                 <div name="botones">
                     <input type="button" name="editar" value="E">
                     <input type="button" name="borrar" value="X">
                 </div>
-            </div> 
+            </div>
         </div>
         <div id="div-btn-agregar">
-            <input type="button" name="agregar" value="Agregar">
+            <input type="button" id="btn-agregar-grupo" name="agregar" value="Agregar con template">
         </div>
     </main>
+
+    <template id="tpl-targeta-grupo">
+        <div name="targeta-grupo">
+            <div name="encabezado">
+                <div class="marco-imagen"></div>
+                <h1 class="nombre-grupo"></h1>
+            </div>
+            <div name="info-grupo">
+                <p><strong>Adscripto/a: </strong></p>
+                <p class="nombre-adscripta">Silvana Salvatierra</p>
+
+                <p><strong>Curso: </strong></p>
+                <p class="nombre-curso">Informática</p>
+            </div>
+            <div name="botones">
+                <input type="button" name="editar" value="E">
+                <input type="button" name="borrar" value="X">
+            </div>
+        </div>
+    </template>
+
+    <script src="../scripts/crear_targeta_grupo.js"></script>
 </body>
+
 </html>
