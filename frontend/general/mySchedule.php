690fcc262789b1b059789f48957ad431cdf08cd4
Nehuel Acosta <nehuelacostarodriguez@gmail.com>
2025-10-26 18:46:43 -0300

    ADD: Funcion crear fila en mySchedule

--- frontend/general/mySchedule.php
+++ frontend/general/mySchedule.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
+
 <body id="body-my-schedule">
     <header class="header">
         <h1 class="titulo">Planilla de Horarios</h1>
@@ -28,68 +30,68 @@
 
                     <tr class="fila">
                         <td class="hora">07:00 - 07:45</td>
-                        <td>Salon 4</td>
-                        <td>3°MD</td>
+                        <td class="espacio">Salon 4</td>
+                        <td class="grupo">3°MD</td>
                     </tr>
 
                     <tr class="fila">
                         <td class="hora">07:50 - 08:35</td>
-                        <td>Salon 5</td>
-                        <td>1° MD</td>
+                        <td class="espacio">Salon 5</td>
+                        <td class="grupo">1° MD</td>
                     </tr>
 
                     <tr class="fila">
                         <td class="hora">08:40 - 09:25</td>
-                        <td></td>
-                        <td></td>
+                        <td class="espacio"></td>
+                        <td class="grupo"></td>
                     </tr>
 
                     <tr class="fila">
                         <td class="hora">09:30 - 10:15</td>
-                        <td></td>
-                        <td></td>
+                        <td class="espacio"></td>
+                        <td class="grupo"></td>
                     </tr>
 
                     <tr class="fila">
                         <td class="hora">10:20 - 11:05</td>
-                        <td></td>
-                        <td></td>
+                        <td class="espacio"></td>
+                        <td class="grupo"></td>
                     </tr>
 
                     <tr class="fila">
                         <td class="hora">11:10 - 11:55</td>
-                        <td></td>
-                        <td></td>
+                        <td class="espacio"></td>
+                        <td class="grupo"></td>
                     </tr>
 
                     <tr class="fila">
                         <td class="hora">12:00 - 12:45</td>
-                        <td></td>
-                        <td></td>
+                        <td class="espacio"></td>
+                        <td class="grupo"></td>
                     </tr>
 
                     <tr class="fila">
                         <td class="hora">12:50 - 13:35</td>
-                        <td></td>
-                        <td></td>
+                        <td class="espacio"></td>
+                        <td class="grupo"></td>
                     </tr>
 
                     <tr class="fila">
                         <td class="hora">13:40 - 14:25</td>
-                        <td></td>
-                        <td></td>
+                        <td class="espacio"></td>
+                        <td class="grupo"></td>
                     </tr>
 
                     <tr class="fila">
                         <td class="hora">14:30 - 15:15</td>
-                        <td></td>
-                        <td></td>
+                        <td class="espacio"></td>
+                        <td class="grupo"></td>
                     </tr>
 
                     <tr class="fila">
                         <td class="hora">15:20 - 16:05</td>
-                        <td></td>
-                        <td></td>
+                        <td class="espacio"></td>
+                        <td class="grupo"></td>
                     </tr>
 
                 </tbody>
@@ -97,13 +99,26 @@
 
             <div class="botones">
                 <button class="btn"><b>Agregar inasistencia</b></button>
-                
+
                 <button class="btn" onclick="location.href='menurecursos.php'">
                     <i class="bi bi-arrow-left"></i>
                 </button>
+
+                <button class="btn" id="btn-agregar-fila"><b>Agregar Fila (template)</b></button>
             </div>
-            
+
         </section>
     </main>
+
+    <template id="tpl-fila">
+        <tr class="fila">
+            <td class="hora"></td>
+            <td class="espacio"></td>
+            <td class="grupo"></td>
+        </tr>
+    </template>
+
+    <script src="../scripts/crear_fila_mySchedule.js"></script>
 </body>
+
 </html>
