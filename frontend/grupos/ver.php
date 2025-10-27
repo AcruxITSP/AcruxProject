019afdbe73da79eccfb4598a953ba29972c38b73
Nehuel Acosta <nehuelacostarodriguez@gmail.com>
2025-10-27 10:27:23 -0300

    ADD: menues agregados en grupos y horarios

--- frontend/grupos/ver.php
+++ frontend/grupos/ver.php
@@ -6,84 +6,92 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ver Grupos</title>
     <link rel="stylesheet" href="../styles/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 
 <body id="body-grupos-ver">
-    <main id="main-grupos-ver">
-        <div class="scrollable-list" id="lista-targetas-grupos">
-            <div name="targeta-grupo">
-                <div name="encabezado">
-                    <div class="marco-imagen"></div>
-                    <h1 class="nombre-grupo">3MD</h1>
-                </div>
-                <div name="info-grupo">
-                    <p><strong>Adscripto/a: </strong></p>
-                    <p class="nombre-adscripta">Silvana Salvatierra</p>
+    <div id="menues">
+        <?php include_once __DIR__ . '/../includes/blockSidebarMenu.php' ?>
+        <?php include_once __DIR__ . '/../includes/blockTopHeader.php' ?>
+    </div>
 
-                    <p><strong>Curso: </strong></p>
-                    <p class="nombre-curso">Informática</p>
+    <div id="main-content">
+        <main id="main-grupos-ver">
+            <div class="scrollable-list" id="lista-targetas-grupos">
+                <div name="targeta-grupo">
+                    <div name="encabezado">
+                        <div class="marco-imagen"></div>
+                        <h1 class="nombre-grupo">3MD</h1>
+                    </div>
+                    <div name="info-grupo">
+                        <p><strong>Adscripto/a: </strong></p>
+                        <p class="nombre-adscripta">Silvana Salvatierra</p>
+
+                        <p><strong>Curso: </strong></p>
+                        <p class="nombre-curso">Informática</p>
+                    </div>
+                    <div name="botones">
+                        <input type="button" name="editar" value="E">
+                        <input type="button" name="borrar" value="X">
+                    </div>
                 </div>
-                <div name="botones">
-                    <input type="button" name="editar" value="E">
-                    <input type="button" name="borrar" value="X">
+                <div name="targeta-grupo">
+                    <div name="encabezado">
+                        <div class="marco-imagen"></div>
+                        <h1 class="nombre-grupo">3MD</h1>
+                    </div>
+                    <div name="info-grupo">
+                        <p><strong>Adscripto/a: </strong></p>
+                        <p class="nombre-adscripta">Silvana Salvatierra</p>
+
+                        <p><strong>Curso: </strong></p>
+                        <p class="nombre-curso">Informática</p>
+                    </div>
+                    <div name="botones">
+                        <input type="button" name="editar" value="E">
+                        <input type="button" name="borrar" value="X">
+                    </div>
+                </div>
+                <div name="targeta-grupo">
+                    <div name="encabezado">
+                        <div class="marco-imagen"></div>
+                        <h1 class="nombre-grupo">3MD</h1>
+                    </div>
+                    <div name="info-grupo">
+                        <p><strong>Adscripto/a: </strong></p>
+                        <p class="nombre-adscripta">Silvana Salvatierra</p>
+
+                        <p><strong>Curso: </strong></p>
+                        <p class="nombre-curso">Informática</p>
+                    </div>
+                    <div name="botones">
+                        <input type="button" name="editar" value="E">
+                        <input type="button" name="borrar" value="X">
+                    </div>
+                </div>
+                <div name="targeta-grupo">
+                    <div name="encabezado">
+                        <div class="marco-imagen"></div>
+                        <h1 class="nombre-grupo">3MD</h1>
+                    </div>
+                    <div name="info-grupo">
+                        <p><strong>Adscripto/a: </strong></p>
+                        <p class="nombre-adscripta">Silvana Salvatierra</p>
+
+                        <p><strong>Curso: </strong></p>
+                        <p class="nombre-curso">Informática</p>
+                    </div>
+                    <div name="botones">
+                        <input type="button" name="editar" value="E">
+                        <input type="button" name="borrar" value="X">
+                    </div>
                 </div>
             </div>
-            <div name="targeta-grupo">
-                <div name="encabezado">
-                    <div class="marco-imagen"></div>
-                    <h1 class="nombre-grupo">3MD</h1>
-                </div>
-                <div name="info-grupo">
-                    <p><strong>Adscripto/a: </strong></p>
-                    <p class="nombre-adscripta">Silvana Salvatierra</p>
-
-                    <p><strong>Curso: </strong></p>
-                    <p class="nombre-curso">Informática</p>
-                </div>
-                <div name="botones">
-                    <input type="button" name="editar" value="E">
-                    <input type="button" name="borrar" value="X">
-                </div>
+            <div id="div-btn-agregar">
+                <input type="button" id="btn-agregar-grupo" name="agregar" value="Agregar con template">
             </div>
-            <div name="targeta-grupo">
-                <div name="encabezado">
-                    <div class="marco-imagen"></div>
-                    <h1 class="nombre-grupo">3MD</h1>
-                </div>
-                <div name="info-grupo">
-                    <p><strong>Adscripto/a: </strong></p>
-                    <p class="nombre-adscripta">Silvana Salvatierra</p>
-
-                    <p><strong>Curso: </strong></p>
-                    <p class="nombre-curso">Informática</p>
-                </div>
-                <div name="botones">
-                    <input type="button" name="editar" value="E">
-                    <input type="button" name="borrar" value="X">
-                </div>
-            </div>
-            <div name="targeta-grupo">
-                <div name="encabezado">
-                    <div class="marco-imagen"></div>
-                    <h1 class="nombre-grupo">3MD</h1>
-                </div>
-                <div name="info-grupo">
-                    <p><strong>Adscripto/a: </strong></p>
-                    <p class="nombre-adscripta">Silvana Salvatierra</p>
-
-                    <p><strong>Curso: </strong></p>
-                    <p class="nombre-curso">Informática</p>
-                </div>
-                <div name="botones">
-                    <input type="button" name="editar" value="E">
-                    <input type="button" name="borrar" value="X">
-                </div>
-            </div>
-        </div>
-        <div id="div-btn-agregar">
-            <input type="button" id="btn-agregar-grupo" name="agregar" value="Agregar con template">
-        </div>
-    </main>
+        </main>
+    </div>
 
     <template id="tpl-targeta-grupo">
         <div name="targeta-grupo">
@@ -106,6 +114,8 @@
     </template>
 
     <script src="../scripts/crear_targeta_grupo.js"></script>
+    <script src="../scripts/menuHamburgesa.js"></script>
+    <script src="../scripts/dropdownMenu.js"></script>
 </body>
 
 </html>
--- frontend/horarios/horas.php
+++ frontend/horarios/horas.php
@@ -6,9 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Definir horas</title>
     <link rel="stylesheet" href="../styles/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 
 <body id="body-horas">
+    <?php include_once __DIR__ . '/../includes/blockSidebarMenu.php' ?>
+    <?php include_once __DIR__ . '/../includes/blockTopHeader.php' ?>
+
     <div id="modal-registrar-hora">
         <div id="main-horas">
             <div id="lista-intervalos">
@@ -49,6 +53,8 @@
     </div>
 
     <script src="../scripts/horarios_horas.js"></script>
+    <script src="../scripts/menuHamburgesa.js"></script>
+    <script src="../scripts/dropdownMenu.js"></script>
 </body>
 
 </html>
