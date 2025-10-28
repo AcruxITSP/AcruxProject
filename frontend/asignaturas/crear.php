5ecd273f64067ed5e3100bf33d107a8877b87737
AlphaSupremo123 <diazthiago4455@gmail.com>
2025-10-28 12:01:54 -0700

    sin terminar

---
+++ frontend/asignaturas/crear.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+    
+
+<div class="asignatura">
+<h1>Crear asignatura</h1>
+
+<label for="asignaturas">Nombre</label>
+
+<input type="text">
+
+</div>
+
+
+
+<div>
+
+
+</div>
+
+
+<div>
+
+
+
+
+
+</div>
+
+<div class="crear">
+
+<label for="crear-asign">Crear</label>
+<input type="text">
+
+</div>
+
+
+
+
+</body>
+</html>
