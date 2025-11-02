e5b64bb623ac7990caec93142ba77e662ed1d1fc
Nehuel Acosta <nehuelacostarodriguez@gmail.com>
2025-11-01 21:28:06 -0300

    ADD: pagina registrar ausencia (sigue desconectada)

--- frontend/ausencias/registrar.php
+++ frontend/ausencias/registrar.php
@@ -9,37 +9,34 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 
-<body class="menues-incluidos">
-    <div id="menues">
-        <?php include_once __DIR__ . '/../includes/blockSidebarMenu.php' ?>
+<body>
+    <?php include_once __DIR__ . '/../includes/blockSidebarMenu.php' ?>
+
+    <main class="main-content" id="main-registrar-ausencia">
         <?php include_once __DIR__ . '/../includes/blockTopHeader.php' ?>
-    </div>
 
-    <div id="main-content">
-        <main id="main-registrar-ausencia">
-            <?php include_once __DIR__ . '/../includes/blockTopHeader.php' ?>
+        <div id="modal-registrar-ausencia">
+            <form class="formulario-basico">
+                <h1>Registrar Ausencia</h1>
+                <label for="fechaInicio">Inicio <input type="date" id="fechaInicio"></label>
+                <label for="fechaFin">Fin <input type="date" id="fechaFin"></label>
 
-            <div id="modal-registrar-ausencia">
-                <form>
-                    <h2>Registrar Ausencia</h2>
-                    <label for="fechaInicio">Inicio <input type="date" id="fechaInicio"></label>
-                    <label for="fechaFin">Fin <input type="date" id="fechaFin"></label>
+                <label>Hora</label>
+                <label for="allDay" id="allday-label">Todo el día <input type="checkbox" id="allDay"></label>
 
-                    <p>Horas</p>
-                    <label for="allDay"><input type="checkbox" id="allDay"> Todo el día</label>
+                <div id="lista-intervalos">
+                    Aca van los intervalos
+                </div>
 
-                    <div id="lista-intervalos">
-                        Aca van los intervalos
-                    </div>
-
-                    <p>Motivo</p>
+                <div>
+                    <label>Motivo</label>
                     <textarea id="input-motivo"></textarea>
+                </div>
 
-                    <input type="submit" value="Registrar">
-                </form>
-            </div>
-        </main>
-    </div>
+                <input type="submit" value="Registrar">
+            </form>
+        </div>
+    </main>
 
     <script src="../scripts/menuHamburgesa.js"></script>
     <script src="../scripts/dropdownMenu.js"></script>
