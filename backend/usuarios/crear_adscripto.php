00a99827454f852235d462ca519e043b1818ad37
AcruxDevelopment <acruxstudios.dev@gmail.com>
2025-11-11 11:51:35 -0300

    crear adscripto

---
+++ backend/usuarios/crear_adscripto.php
@@ -0,0 +1,67 @@
+<?php
+@session_start();
+require_once dirname(__FILE__).'/../other/connection.php';
+require_once dirname(__FILE__).'/../other/sql.php';
+require_once dirname(__FILE__).'/../other/time.php';
+require_once dirname(__FILE__).'/../other/respuestas.php';
+require_once dirname(__FILE__).'/../other/db_errors.php';
+require_once dirname(__FILE__).'/../util/strings.php';
+
+// PUEDE TIRAR LOS CÓDIGOS DE ERROR:
+// - NECESITA_LOGIN
+// - NECESITA_CI
+// - NECESITA_NOMBRE
+// - NECESITA_APELLIDO
+// - NECESITA_CONTRASENA
+// - NECESITA_EMAIL
+// - USUARIO_EXISTE
+// - NECESITA_MATERIAS
+
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    if(!isset($_SESSION['id_usuario'])) Respuestas::enviarError("NECESITA_LOGIN");
+
+    if(!isset($_POST['ci'])) Respuestas::enviarError("NECESITA_CI");
+    if(!isset($_POST['nombre'])) Respuestas::enviarError("NECESITA_NOMBRE");
+    if(!isset($_POST['apellido'])) Respuestas::enviarError("NECESITA_APELLIDO");
+    if(!isset($_POST['contrasena'])) Respuestas::enviarError("NECESITA_CONTRASENA");
+    if(!isset($_POST['email'])) Respuestas::enviarError("NECESITA_EMAIL");
+
+    $ci = $_POST['ci'];
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $contrasena = $_POST['contrasena'];
+    $email = $_POST['email'];
+    $materias = $_POST['materias'] ?? [];
+
+    // Encripta la contraseña
+    $hash = password_hash($contrasena, PASSWORD_BCRYPT);
+
+    $con = connectDb();
+    $con->begin_transaction();
+
+    // Verificar existencia previa
+    $sql = "SELECT id_usuario FROM Usuario WHERE ci = ? OR email = ?";
+    $check = SQL::valueQuery($con, $sql, "ss", $ci, $email);
+    if($check instanceof ErrorDB) Respuestas::enviarError($check, $con);
+
+    if($check->num_rows > 0)
+        Respuestas::enviarError("USUARIO_EXISTE", $con);
+
+    // Crear usuario
+    $sql = "INSERT INTO Usuario(ci, nombre, apellido, contrasena, email) VALUES (?, ?, ?, ?, ?)";
+    $result = SQL::actionQuery($con, $sql, "sssss", $ci, $nombre, $apellido, $hash, $email);
+    if($result instanceof ErrorDB) Respuestas::enviarError($result, $con);
+
+    $id_usuario = $con->insert_id;
+
+    // Crear profesor
+    $sql = "INSERT INTO Adscrito(id_usuario) VALUES (?)";
+    $result = SQL::actionQuery($con, $sql, "i", $id_usuario);
+    if($result instanceof ErrorDB) Respuestas::enviarError($result, $con);
+
+    $id_adscripto = $con->insert_id;
+
+    Respuestas::enviarOk(["id_usuario" => $id_usuario, "id_adscripto" => $id_adscripto], $con);
+}
+?>
