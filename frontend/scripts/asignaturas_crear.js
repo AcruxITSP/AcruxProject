319059a638e56a20a9a786c82cfbe761fd315694
Nehuel Acosta <nehuelacostarodriguez@gmail.com>
2025-11-11 20:13:08 -0300

    ADD: formularios de asignaturas funcionan (faltan los inputs(

--- frontend/scripts/asignaturas_crear.js
+++ frontend/scripts/asignaturas_crear.js
@@ -4,6 +4,8 @@ const domDivOpcionesProfesores = document.getElementById("opcionesProfesores");
 const domLabelOpcionesCursos = document.getElementById("label-opcionesCursos");
 const domDivOpcionesCursos = document.getElementById("opcionesCursos");
 
+const form = document.getElementById("form-crear-asignatura");
+
 /* Array con datos de profesores. Ejemplo */
 const jsonStringProfesores = '[{"id_profesor": "1", "nombre": "Juan", "apellido": "Carlos"}, {"id_profesor": "2", "nombre": "Pancho", "apellido": "Gomez"}, {"id_profesor": "3", "nombre": "Fabian", "apellido": "Sosa"}]';
 const profesores = JSON.parse(jsonStringProfesores);
@@ -33,7 +35,7 @@ function listaProfesoresOptions(profesores){
 
         input.value = `${profesor.id_profesor}`;
         input.type = "checkbox";
-        input.name = "id_profesor[]"
+        input.name = "id_profesores[]"
 
         label.appendChild(input);
         label.appendChild(nodeProfesor);
@@ -51,7 +53,7 @@ function listaCursosOptions(cursos) {
 
         input.value = `${curso.id_curso}`;
         input.type = "checkbox";
-        input.name = "id_curso[]"
+        input.name = "id_cursos[]"
 
         label.appendChild(input);
         label.appendChild(nodeCurso);
@@ -64,3 +66,38 @@ function listaCursosOptions(cursos) {
 
 listaProfesoresOptions(profesores);
 listaCursosOptions(cursos);
+
+/* Envio del formulario */
+
+form.addEventListener("submit", async e => {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    let respuesta = await fetch(`../../../backend/asignaturas/crear.php`, { method: "POST", body: formData });
+    respuesta = await respuesta.json();
+
+    if (respuesta.ok) {
+        await Swal.fire({
+            title: "Espacio Creado",
+            text: `El espacio ha sido creada exitosamente`,
+            icon: "success"
+        });
+    }
+    else {
+        switch (respuesta.value) {
+            case "NECESITA_LOGIN":
+                Swal.fire({
+                    title: "Login Requerido",
+                    text: `Necesitas iniciar sesion para crear un espacio.`,
+                    icon: "error"
+                });
+                break;
+            default:
+                Swal.fire({
+                    title: "Error Desconocido",
+                    text: `Un error desconocido ha ocurrido`,
+                    icon: "error"
+                });
+        }
+    }
+});
--- frontend/scripts/asignaturas_editar.js
+++ frontend/scripts/asignaturas_editar.js
@@ -4,6 +4,8 @@ const domDivOpcionesProfesores = document.getElementById("opcionesProfesores");
 const domLabelOpcionesCursos = document.getElementById("label-opcionesCursos");
 const domDivOpcionesCursos = document.getElementById("opcionesCursos");
 
+const form = document.getElementById("form-editar-asignatura");
+
 /* Array con datos de profesores. Ejemplo */
 const jsonStringProfesores = '[{"id_profesor": "1", "nombre": "Juan", "apellido": "Carlos"}, {"id_profesor": "2", "nombre": "Pancho", "apellido": "Gomez"}, {"id_profesor": "3", "nombre": "Fabian", "apellido": "Sosa"}]';
 const profesores = JSON.parse(jsonStringProfesores);
@@ -24,7 +26,7 @@ domLabelOpcionesProfesores.addEventListener("click", () => {
     domDivOpcionesCursos.classList.remove("show");
 });
 
-function listaProfesoresOptions(profesores){
+function listaProfesoresOptions(profesores) {
     profesores.forEach(profesor => {
         const label = document.createElement("label");
         const input = document.createElement("input");
@@ -33,7 +35,7 @@ function listaProfesoresOptions(profesores){
 
         input.value = `${profesor.id_profesor}`;
         input.type = "checkbox";
-        input.name = "id_profesor[]"
+        input.name = "id_profesores[]"
 
         label.appendChild(input);
         label.appendChild(nodeProfesor);
@@ -51,7 +53,7 @@ function listaCursosOptions(cursos) {
 
         input.value = `${curso.id_curso}`;
         input.type = "checkbox";
-        input.name = "id_curso[]"
+        input.name = "id_cursos[]"
 
         label.appendChild(input);
         label.appendChild(nodeCurso);
@@ -60,7 +62,47 @@ function listaCursosOptions(cursos) {
     });
 }
 
-/* Llamar a las funciones */
+/* Llamar a las funciones para los campos del formulario */
 
 listaProfesoresOptions(profesores);
 listaCursosOptions(cursos);
+
+/* Envio del formulario */
+
+form.addEventListener("submit", async e => {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    const urlParams = new URLSearchParams(window.location.search); //trae los parametros de la url
+    const id = urlParams.get("id"); // agarra el id de la url
+
+    formData.append("id_materia", id);
+
+    let respuesta = await fetch(`../../../backend/asignaturas/editar.php`, { method: "POST", body: formData });
+    respuesta = await respuesta.json();
+
+    if (respuesta.ok) {
+        await Swal.fire({
+            title: "Espacio Creado",
+            text: `El espacio ha sido creada exitosamente`,
+            icon: "success"
+        });
+    }
+    else {
+        switch (respuesta.value) {
+            case "NECESITA_LOGIN":
+                Swal.fire({
+                    title: "Login Requerido",
+                    text: `Necesitas iniciar sesion para crear un espacio.`,
+                    icon: "error"
+                });
+                break;
+            default:
+                Swal.fire({
+                    title: "Error Desconocido",
+                    text: `Un error desconocido ha ocurrido`,
+                    icon: "error"
+                });
+        }
+    }
+});
