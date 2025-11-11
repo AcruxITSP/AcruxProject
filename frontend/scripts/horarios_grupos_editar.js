ac868ba30016e799ac93c63d1a985fa04c54ce9d
Nehuel Acosta <nehuelacostarodriguez@gmail.com>
2025-11-11 20:59:00 -0300

    WHIP pmo

--- frontend/scripts/horarios_grupos_editar.js
+++ frontend/scripts/horarios_grupos_editar.js
@@ -2,6 +2,8 @@ const domInputSelectMateria = document.getElementById("select-materia");
 const domInputSelectProfesor = document.getElementById("select-profesor");
 const domInputSelectEspacio = document.getElementById("select-espacio");
 
+const form = document.getElementById("form-editar");
+
 /* Array de materias de ejemplo */
 const jsonStringMaterias = '[{"id_materia": "1", "nombre": "Programacion"}, {"id_materia": "2", "nombre": "Ciberseguridad" }, {"id_materia": "3", "nombre": "Biologia"}, {"id_materia": "4", "nombre": "Fisica"}, {"id_materia": "5", "nombre": "Logica"}, {"id_materia": "6", "nombre": "utulab"}, {"id_materia": "7", "nombre": "Sistemas Operativos"}, {"id_materia": "8", "nombre": "Filosofia"}, {"id_materia": "9", "nombre": "Sociologia"}]';
 const materias = JSON.parse(jsonStringMaterias);
@@ -16,7 +18,7 @@ const espacios = JSON.parse(jsonStringEspacios);
 
 /* Funciones */
 
-function listaMateriasOptions(materias){
+function listaMateriasOptions(materias) {
     materias.forEach(materia => {
         const option = document.createElement("option");
 
@@ -27,7 +29,7 @@ function listaMateriasOptions(materias){
     });
 }
 
-function listaProfesoresOptions(profesores){
+function listaProfesoresOptions(profesores) {
     profesores.forEach(profesor => {
         const option = document.createElement("option");
 
@@ -38,7 +40,7 @@ function listaProfesoresOptions(profesores){
     });
 }
 
-function listaEspaciosOptions(espacios){
+function listaEspaciosOptions(espacios) {
     espacios.forEach(espacio => {
         const option = document.createElement("option");
 
@@ -52,3 +54,56 @@ function listaEspaciosOptions(espacios){
 listaMateriasOptions(materias);
 listaProfesoresOptions(profesores);
 listaEspaciosOptions(espacios);
+
+/* Envio del formulario */
+
+form.addEventListener("submit", async e => {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    const urlParams = new URLSearchParams(window.location.search); //trae los parametros de la url
+    const id = urlParams.get("id"); // agarra el id de la url
+
+    formData.append("id", id);
+
+    for (var pair of formData.entries()) {
+        console.log(pair[0] + ', ' + pair[1]);
+    }
+
+    let respuesta = await fetch(`../../backend/horarios/grupos_editar_modulo.php`, { method: "POST", body: formData });
+    respuesta = await respuesta.json();
+
+    if (respuesta.ok) {
+        await Swal.fire({
+            title: "Módulo Actualizado",
+            text: `La información del módulo fue actualizada exitosamente`,
+            icon: "success"
+        });
+    }
+    else {
+        switch (respuesta.value) {
+            case "NECESITA_LOGIN":
+                Swal.fire({
+                    title: "Login Requerido",
+                    text: `Necesitas iniciar sesión para realizar esta acción.`,
+                    icon: "error"
+                });
+                break;
+
+            case "ID_ESPACIO_INVALIDA":
+                Swal.fire({
+                    title: "Módulo No Encontrado",
+                    text: `No se ha seleccionado ningún módulo o la id no existe.`,
+                    icon: "error"
+                });
+                break;
+
+            default:
+                Swal.fire({
+                    title: "Error Desconocido",
+                    text: `Un error desconocido ha ocurrido`,
+                    icon: "error"
+                });
+        }
+    }
+});
