b4b8029414251e400472a972ac577fbb0a9afd8b
Nehuel Acosta <nehuelacostarodriguez@gmail.com>
2025-11-13 00:11:22 -0300

    WHIP: Testo de ruta dinamica en el servidor

---
+++ frontend/includes/rutaBase.php
@@ -0,0 +1,11 @@
+<?php
+
+$regex = '/xampp/'; 
+$ruta = __DIR__;
+
+if (preg_match_all($regex, $ruta, $coincidencias)) {
+    define('BASE_PATH', "/frontend");
+} else {
+    define('BASE_PATH', "/Acrux/AcruxProject/frontend");
+}
+
